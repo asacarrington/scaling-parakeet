d5d401d02591d278a56724d7f95607ae261835ff
Carrington <asa.carrington@mccann.com>
2016-06-10 15:59:45 +0100

    google tracking basic impl

--- asacarrington.cms/scripts/analytics/analytics.js
+++ asacarrington.cms/scripts/analytics/analytics.js
@@ -1,3 +1,25 @@
 ﻿$(document).ready(function () {
     console.log("ready!");
+
+    ga('send', {
+        hitType: 'event',
+        eventCategory: 'Videos',
+        eventAction: 'play',
+        eventLabel: 'Fall Campaign'
+    });
+
+    ga('send', {
+        hitType: 'social',
+        socialNetwork: 'Facebook',
+        socialAction: 'like',
+        socialTarget: 'http://myownpersonaldomain.com'
+    });
+
+    ga('send', 'screenview', {
+        'screenName': 'Home'
+    });
+
+    console.log("yea!");
+
+
 });
