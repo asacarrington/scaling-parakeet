f8db0e09fdb7c377efbf9df4918461dad2c95d5e
asa.carrington <asa.carrington@MANMEW-LW766S5>
2016-06-14 15:42:49 +0100

    added front end workflow with gulp and npm

--- asacarrington.cms/scripts/analytics/analytics.js
+++ asacarrington.cms/scripts/analytics/analytics.js
@@ -1,25 +1,43 @@
 ﻿$(document).ready(function () {
-    console.log("ready!");
 
-    ga('send', {
-        hitType: 'event',
-        eventCategory: 'Videos',
-        eventAction: 'play',
-        eventLabel: 'Fall Campaign'
+    initializeGA();
+
+    $(".social-track").on("click", function () {
+        ga('send', {
+            hitType: 'social',
+            socialNetwork: $(this).data("social-network"),
+            socialAction: $(this).data("social-action"),
+            socialTarget: $(this).data("social-target")
+        });
     });
 
-    ga('send', {
-        hitType: 'social',
-        socialNetwork: 'Facebook',
-        socialAction: 'like',
-        socialTarget: 'http://myownpersonaldomain.com'
+    $(".event-track").on("click", function () {
+        ga('send', {
+            hitType: 'event',
+            eventCategory: $(this).data("event-category"),
+            eventAction: $(this).data("event-action"),
+            eventLabel: $(this).data("event-lable")
+        });
     });
 
-    ga('send', 'screenview', {
-        'screenName': 'Home'
-    });
+    function TriggerPageView() {
+        ga('send', {
+            hitType: 'pageview',
+            page: location.pathname,
+            location: 'MyLocation',
+            title: 'myTItle'
+        });
+    }
 
-    console.log("yea!");
+    function TriggerScreenView() {
+        ga('send', 'screenview', {
+            'screenName': 'Home'
+        });
+    }
 
+    function initializeGA() {
+        ga('create', 'UA-79172520-1', 'auto');
+        TriggerPageView();
+    }
 
 });
